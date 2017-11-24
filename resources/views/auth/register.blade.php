51da4b508b6bbd988e2dbb6dcc4878df2de485a4
dessHub <deskip95@gmail.com>
2017-11-24 23:30:58 +0300

    designed property page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,9 +12,9 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="name" class="col-md-4 col-md-offset-6 control-label">Name</label>
 
-                            <div class="col-md-6">
+                            <div class=" col-md-offset-2 col-md-8">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -26,9 +26,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4  col-md-offset-6 control-label">E-Mail Address</label>
 
-                            <div class="col-md-6">
+                            <div class=" col-md-offset-2 col-md-8">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -40,9 +40,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4  col-md-offset-6 control-label">Password</label>
 
-                            <div class="col-md-6">
+                            <div class=" col-md-offset-2 col-md-8">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -54,16 +54,16 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 col-md-offset-6 control-label">Confirm Password</label>
 
-                            <div class="col-md-6">
+                            <div class=" col-md-offset-2 col-md-8">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-offset-4 col-md-6 ">
+                                <button type="submit" class="btn btn-default pull-right">
                                     Register
                                 </button>
                             </div>
--- resources/views/properties/property.blade.php
+++ resources/views/properties/property.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 
-
 @section('content')
 
 <div class="container">
@@ -45,14 +44,80 @@ The building offers a superb amenity package th...at includes a round the clock 
   </div>
   </div>
   <!-- Property Details Ends -->
-  <div class="col-md-4 col-xs-12" style="margin-top:-35px;">
+  <div class="col-md-4 col-xs-12" style="margin-top:-3px;">
    <div class="page-header"><h3>Leave a Message</h3></div>
    <div class="property-contact">
+       <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+           {{ csrf_field() }}
 
+           <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+               <label for="name" class="col-md-12 control-label">Name</label>
+
+               <div class="col-md-12">
+                   <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                   @if ($errors->has('name'))
+                       <span class="help-block">
+                           <strong>{{ $errors->first('name') }}</strong>
+                       </span>
+                   @endif
+               </div>
+           </div>
+
+           <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+               <label for="phone" class="col-md-12 control-label">Mobile No</label>
+
+               <div class="col-md-12">
+                   <input id="phone" type="text" class="form-control" name="phone" value="{{ old('phone') }}" required autofocus>
+
+                   @if ($errors->has('phone'))
+                       <span class="help-block">
+                           <strong>{{ $errors->first('phone') }}</strong>
+                       </span>
+                   @endif
+               </div>
+           </div>
+
+           <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+               <label for="email" class="col-md-12 control-label">E-Mail Address</label>
+
+               <div class="col-md-12">
+                   <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                   @if ($errors->has('email'))
+                       <span class="help-block">
+                           <strong>{{ $errors->first('email') }}</strong>
+                       </span>
+                   @endif
+               </div>
+           </div>
+
+           <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
+               <label for="message" class="col-md-12 control-label">Message</label>
+
+               <div class="col-md-12">
+                   <textarea id="message" type="text" class="form-control" name="message" rows="5" value="{{ old('message') }}" required autofocus></textarea>
+
+                   @if ($errors->has('message'))
+                       <span class="help-block">
+                           <strong>{{ $errors->first('message') }}</strong>
+                       </span>
+                   @endif
+               </div>
+           </div>
+
+           <div class="form-group">
+               <div class="col-md-6 col-md-offset-4">
+                   <button type="submit" class="btn btn-primary">
+                       Submit
+                   </button>
+               </div>
+           </div>
+       </form>
    </div>
   </div>
 
-  <!-- Contact Form Starts -->
+  <!-- Contacnt Form Starts -->
   </div>
 </div>
 </div>
